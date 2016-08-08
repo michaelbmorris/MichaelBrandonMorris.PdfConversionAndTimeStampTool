f5c181c290120eabf9763cad75779211bf194c1b
Michael Brandon Morris <brandon@michaelbmorris.com>
2016-08-08 13:59:22 -0400

    Removed old folders and files.

--- MichaelBrandonMorris.PdfConversionAndTimeStampTool/Properties/AssemblyInfo.cs
+++ MichaelBrandonMorris.PdfConversionAndTimeStampTool/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MichaelBrandonMorris.PdfConversionAndTimeStampTool")]
+[assembly: AssemblyTitle("PdfConversionAndTimeStampTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MichaelBrandonMorris.PdfConversionAndTimeStampTool")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("Michael Brandon Morris")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © Michael Brandon Morris 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
